b6efece379cd3c5e0f0cc10683b5fcc9408ba474
oLNidfwworld <kuvetov@bk.ru>
2023-04-03 15:19:21 +0300

    1/2 main gape

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,29 @@
 import '../scss/app.scss';
-     
+import Swiper, {FreeMode} from 'swiper';
+
+new Swiper('.banners__wrapper', {
+  slidesPerView: 1,
+  breakpoints: {
+    0: {
+      spaceBetween: 20,
+    },
+    1024: {
+      spaceBetween: 0,
+    },
+  },
+});
+new Swiper('.storys', {
+  modules: [FreeMode],
+  freeMode: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 3,
+    },
+    480: {
+      slidesPerView: 4,
+    },
+    720: {
+      slidesPerView: 5,
+    },
+  },
+});
