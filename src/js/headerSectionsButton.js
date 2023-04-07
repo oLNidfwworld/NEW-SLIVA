2343ef6ac5bdee3c549129efeb97d9f2556ca834
oLNidfwworld <kuvetov@bk.ru>
2023-04-07 14:18:15 +0300

    Каталог надо доделать или переделать

--- src/js/headerSectionsButton.js
+++ src/js/headerSectionsButton.js
@@ -1,9 +1,17 @@
 export function headerSectionsButton() {
   const leftBlockSections = document.querySelectorAll('.header-sections__1lvl-item');
+  const buttonOpenCatalog = document.querySelector('#button-open-sections');
+  const headerSections = document.querySelector('.header-sections');
+  buttonOpenCatalog.addEventListener('click',()=>{
+    headerSections.classList.toggle('block');
+  })
   leftBlockSections.forEach((el) => {
     el.addEventListener('mouseover', () => {
       document.querySelector('.header-sections__2lvl-wrapper.header-sections__2lvl_current').classList.remove('header-sections__2lvl_current');
       document.querySelector(`*[data-section-id="${el.dataset.subsectionId}"]`).classList.add('header-sections__2lvl_current');
+
+      document.querySelector('.header-sections__1lvl-item.header-sections__1lvl-item-current').classList.remove('header-sections__1lvl-item-current');
+      el.classList.add('header-sections__1lvl-item-current')
     });
   });
 }
